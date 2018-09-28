faf0257bf3ef2e91f912e3a9e9baa431960f5bda
DeboraArzu <deboranise29@hotmail.com>
2018-09-28 16:58:02 -0600

    path added

--- Lab 4/App.js
+++ Lab 4/App.js
@@ -1,32 +1,12 @@
-const http = require('http');
-url = require('url');
+const express = require('express')
 
-//create the http server accepting requests to port 3333
-http.createServer(function (req, res) {
-    //get url infomation
-    var urlParts = url.parse(req.url);
-    console.log(req.url, urlParts);
- 
-    //direct the request to appropriate function to be processed based on the url pathname
-    switch(urlParts.pathname) {
-        case "/":
-            homepage(req, res);
-            break;
-        case "/hello/:name":
-            read(req, res);
-            break;
-        default:
-            homepage(req,res);
-            break;
-    }
-}).listen(3000); 
-console.log("Server running at http://localhost:3000/");
+const app = express()
 
-//functions to process incoming requests
-function homepage(req, res) {
-  res.end("Hello, this is the home page :)"); 
-}
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name
+    res.send('Hello' + name)
+})
 
-function read(req, res) {
-  res.end("Hello:<name>");    
-}
+app.listen(3000, () => {
+    console.log("Running in http://localhost:3000")
+})
