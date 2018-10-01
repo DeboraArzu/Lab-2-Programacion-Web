debb01500dd61c6f5fc32a340f1226e6e837103e
DeboraArzu <deboranise29@hotmail.com>
2018-10-01 10:05:31 -0600

    Fixes (Lab 4).

--- Lab 4/app.js
+++ Lab 4/app.js
@@ -1,14 +1,33 @@
-const http = require('http');
+const express = require('express')
+const app = express()
+var http = require('http');
+var formidable = require('formidable');
 
-const hostname = '127.0.0.1';
-const port = 3000;
+http.createServer(function (req, res) {
+    if (req.url == '/fileupload') {
+      var form = new formidable.IncomingForm();
+      form.parse(req, function (err, fields, files) {
+        res.write('File uploaded');
+        res.end();
+      });
+    } else {
+      res.writeHead(200, {'Content-Type': 'text/html'});
+      res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
+      res.write('<input type="file" name="filetoupload"><br>');
+      res.write('<input type="submit">');
+      res.write('</form>');
+      return res.end();
+    }
+  }).listen(3000); 
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
+app.get('/hello/:name', (req, res) => {
+    const name = req.params.name
+    res.send('Hello' + name)
+})
+app.get('', (req, res) =>{
+  res.send('Lab-4  By Debora Arzu')
+})
+app.listen(3000, () => {
+    console.log("Running in http://localhost:3000")
+})
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
